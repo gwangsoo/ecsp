30a56bf176bc9985a8fb142dcff79e98b3639d0f
김유진 <>
2024-05-10 15:45:12 +0900

    Revert "Revert "feat: OCPI 2.2.1 Token, Commands 모듈 Entity, Enum 생성""
    This reverts commit b5bb9fb94f9ca72f047fa830fc5ce7a42253a460.

---
+++ bfi/bfi-share/src/main/java/com/example/bfi/domain/dto/enumeration/CommandResponseType.java
@@ -0,0 +1,12 @@
+package com.example.bfi.domain.dto.enumeration;
+
+/**
+ * Commands 모듈
+ * CPO command 요청에 대한 응답
+ */
+public enum CommandResponseType {
+    NOT_SUPPORTED,
+    REJECTED,
+    ACCEPTED,
+    UNKNOWN_SESSION
+}
